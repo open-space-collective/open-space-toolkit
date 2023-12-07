34a357bcad0c741ffbaef9cf223bfd5319992970
Antoine Paletta <98616558+apaletta3@users.noreply.github.com>
2023-12-07 13:10:49 +0100

    chore: update check-format-python.sh

--- tools/development/helpers/check-format-python.sh
+++ tools/development/helpers/check-format-python.sh
@@ -6,6 +6,6 @@ project_directory="$(git rev-parse --show-toplevel)"
 
 pushd "${project_directory}" > /dev/null
 
-    python3.11 -m black --line-length=90 --check --diff bindings/python/
+    python3.11 -m black --check --diff bindings/python/
 
 popd > /dev/null
